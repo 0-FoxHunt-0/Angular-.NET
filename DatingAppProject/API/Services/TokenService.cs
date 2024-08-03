9be575d46c7b9fb8ca791a889d9f3ff858213975
Nate Stahlberg <nethanelstahlberg@gmail.com>
2024-08-03 13:12:43 +0300

    Removed appsettings.json
    Also partial implementation of JWT service and Interface

---
+++ DatingAppProject/API/Services/TokenService.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from appsettings");
+        if (tokenKey.Length < 64) throw new Exception("Your tokenKey needs to be longer");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+    }
+}
--- DatingAppProject/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingAppProject/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ffced65e8d6d7f35caf30b9e1b071eda72d07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea3e718da8e08f45d3c982ff5c2f447b5da2f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
