fef5dae2cd369162b7393fd71a4ad802629c1679
Nate Stahlberg <nethanelstahlberg@gmail.com>
2024-08-12 16:43:31 +0300

    End of section 5

--- DatingAppProject/client/src/app/register/register.component.ts
+++ DatingAppProject/client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, input, Output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -9,11 +10,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  usersFromHomeComponent = input.required<any>();
-  @Output() cancelRegister = new EventEmitter();
+  private accountService = inject(AccountService);
+  cancelRegister = output<boolean>();
   model: any = {};
 
   register() {
+    this.accountService.register(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.cancel();
+      },
+      error: (error) => console.log(error),
+    });
     console.log(this.model);
   }
 
