39d2206a249a7ad0cd85c8a7ddeb52e760253277
Nate Stahlberg <nethanelstahlberg@gmail.com>
2024-08-17 18:20:48 +0300

    End of section 7

--- DatingAppProject/client/src/app/app.config.ts
+++ DatingAppProject/client/src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './_interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(), // Adding animations
     provideToastr({
       positionClass: 'toast-bottom-right',
